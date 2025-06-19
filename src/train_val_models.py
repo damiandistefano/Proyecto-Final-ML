1832b16fa9a48012b3b6477c4100b8d682ba1329
damiandistefano <143121853+damiandistefano@users.noreply.github.com>
2025-06-19 11:09:25 -0300

    .
    dan muy mal las metricas de la regresion lineal, nose si es un problema de la normalizacion o que

--- src/train_val_models.py
+++ src/train_val_models.py
@@ -11,32 +11,27 @@ def split_dataset(df, target_column="Precio_usd", test_size=0.2, random_state=42
 
 import pandas as pd
 
+import pandas as pd
+
 def NormalizeFeatures(df: pd.DataFrame):
-    """
-    Normaliza solo columnas numéricas (int, float) de un DataFrame.
-    Retorna el DataFrame normalizado, además de las medias y desviaciones estándar para usar luego.
-    """
     df_norm = df.copy()
     num_cols = df_norm.select_dtypes(include=["int64", "float64"]).columns.tolist()
-
-    means = df_norm[num_cols].mean()
-    stds = df_norm[num_cols].std()
-
-    df_norm[num_cols] = (df_norm[num_cols] - means) / stds
+    cols_to_normalize = [col for col in num_cols if df_norm[col].nunique() > 2]
+    means = df_norm[cols_to_normalize].mean()
+    stds = df_norm[cols_to_normalize].std()
+    df_norm[cols_to_normalize] = (df_norm[cols_to_normalize] - means) / stds
 
     return df_norm, means, stds
 
-
 def NormalizeWithStats(df: pd.DataFrame, means: pd.Series, stds: pd.Series):
-    """
-    Normaliza usando medias y desviaciones estándar previamente calculadas.
-    """
     df_norm = df.copy()
     num_cols = means.index.tolist()
     df_norm[num_cols] = (df_norm[num_cols] - means) / stds
+
     return df_norm
 
-def train_val_linear_reg(X_train, X_test, y_train, y_test, metodo="pinv", reg=None, lr=0.01, epochs=1000, l1=0, l2=0):
+
+def train_pred_linear_reg(X_train, X_test, y_train, y_test, metodo="pinv", reg=None, lr=0.01, epochs=1000, l1=0, l2=0):
     modelo = LinearReg(X_train, y_train, l1=l1, l2=l2)
 
     if metodo == "pinv":
